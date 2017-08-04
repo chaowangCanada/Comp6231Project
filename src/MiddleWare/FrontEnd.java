d0394deb503d3be6b87b05e58a193fad373aeccf
chaowangcanada <chaowang.ubc@gmail.com>
2017-08-04 07:33:19 -0400

    modified

--- src/MiddleWare/FrontEnd.java
+++ src/MiddleWare/FrontEnd.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Properties;
 import java.util.Queue;
+import java.util.Stack;
 
 import org.omg.CORBA.ORB;
 import org.omg.CosNaming.NameComponent;
@@ -25,7 +26,8 @@ public class FrontEnd extends DCMSPOA{
 		public  ArrayList<Replica> replicaList = new ArrayList<Replica>();
 		private Replica replica1, replica2, replica3;
 		private Replica leader;
-		private Queue<String> requestQ_MTL = new LinkedList<String>();
+		private Queue<String> requestQ = new LinkedList<String>();
+		private Stack<String> processStack = new Stack<String>();
 		private ORB orb;
 		
 		public FrontEnd() throws IOException{
@@ -102,201 +104,112 @@ public class FrontEnd extends DCMSPOA{
 
 			System.out.println("front end Exiting ...");
 		}
-		
+	
+
 		/**
 		 * depend on managerID, dicdie to call which center server.
 		 */
 		@Override
 		public String createTRecord(String managerId, String firstName, String lastName, String address, String phone,
-				String specialization, String location)  {
+				String specialization, String location) {
+			String request = "";
 			if(managerId.substring(0, 3).equalsIgnoreCase("mtl")){
-				return mtl.createTRecord(managerId, firstName, lastName, address, phone, specialization, location);
+				request = "mtl|" + "CT" + "|" + managerId + "|"  + firstName + "|" + lastName + "|" + address + "|" + phone + "|" + specialization + "|" + location;
 			}
 			else if(managerId.substring(0, 3).equalsIgnoreCase("lvl")){
-				return lvl.createTRecord(managerId, firstName, lastName, address, phone, specialization, location);
+				request = "lvl|" + "CT" + "|" + managerId + "|"  + firstName + "|" + lastName + "|" + address + "|" + phone + "|" + specialization + "|" + location;
 			}
 			else if(managerId.substring(0, 3).equalsIgnoreCase("ddo")){
-				return ddo.createTRecord(managerId, firstName, lastName, address, phone, specialization, location);
+				request = "ddo|" + "CT" + "|" + managerId + "|"  + firstName + "|" + lastName + "|" + address + "|" + phone + "|" + specialization + "|" + location;
 			}
-			return "error of finding server";
+			requestQ.add(request);
+
+			return "request queued";
 		}
-
-
+	
+	
 		/**
 		 * depend on managerID, dicdie to call which center server.
 		 */
 		@Override
 		public String createSRecord(String managerId, String firstName, String lastName, String courseRegistered,
-				String status, String statusdate) {
+				String status, String statusdate) throws IOException {
 			if(managerId.substring(0, 3).equalsIgnoreCase("mtl")){
-				return mtl.createSRecord(managerId, firstName, lastName, courseRegistered, status, statusdate);
+				String request = "mtl|" + "CS" + "|" + managerId + "|"  + firstName + "|" + lastName + "|" + courseRegistered + "|" + status + "|" + statusdate;
+				requestQ_MTL.add(request);
 			}
 			else if(managerId.substring(0, 3).equalsIgnoreCase("lvl")){
-				return lvl.createSRecord(managerId, firstName, lastName, courseRegistered, status, statusdate);
+				String request = "lvl|" + "CS" + "|" + managerId + "|"  + firstName + "|" + lastName + "|" + courseRegistered + "|" + status + "|" + statusdate ;
+				requestQ_LVL.add(request);
 			}
 			else if(managerId.substring(0, 3).equalsIgnoreCase("ddo")){
-				return ddo.createSRecord(managerId, firstName, lastName, courseRegistered, status, statusdate);
+				String request = "ddo|" + "CS" + "|" + managerId + "|"  + firstName + "|" + lastName + "|" + courseRegistered + "|" + status + "|" + statusdate;
+				requestQ_DDO.add(request);
 			}
-			return "error of finding server";
+			return "request queued";
 		}
-
-
+	
+	
 		/**
 		 * depend on managerID, dicdie to call which center server.
 		 */
 		@Override
-		public String getRecordCounts(String managerId) {
+		public String getRecordCounts(String managerId) throws IOException {
 			if(managerId.substring(0, 3).equalsIgnoreCase("mtl")){
-				return mtl.getRecordCounts(managerId);
+				String request = "mtl|" + "RC" + "|" + managerId ;
+				requestQ_MTL.add(request);
 			}
 			else if(managerId.substring(0, 3).equalsIgnoreCase("lvl")){
-				return lvl.getRecordCounts(managerId);
+				String request = "lvl|" + "RC" + "|" + managerId ;
+				requestQ_LVL.add(request);
 			}
 			else if(managerId.substring(0, 3).equalsIgnoreCase("ddo")){
-				return ddo.getRecordCounts(managerId);
+				String request = "ddo|" + "RC" + "|" + managerId ;
+				requestQ_DDO.add(request);
 			}
-			return "error of finding server";
+			return "request queued";
 		}
-
-
+	
+	
 		/**
 		 * depend on managerID, dicdie to call which center server.
 		 */
 		@Override
-		public String editRecord(String managerId, String recordID, String filedname, String newValue) {
+		public String editRecord(String managerId, String recordID, String filedname, String newValue) throws IOException {
 			if(managerId.substring(0, 3).equalsIgnoreCase("mtl")){
-				return mtl.editRecord(managerId, recordID, filedname, newValue);
+				String request = "mtl|" + "ER" + "|" + managerId + "|"  + recordID + "|" + filedname + "|" + newValue ;
+				requestQ_MTL.add(request);
 			}
 			else if(managerId.substring(0, 3).equalsIgnoreCase("lvl")){
-				return lvl.editRecord(managerId, recordID, filedname, newValue);
+				String request = "lvl|" + "ER" + "|" + managerId + "|"  + recordID + "|" + filedname + "|" + newValue ;
+				requestQ_LVL.add(request);
 			}
 			else if(managerId.substring(0, 3).equalsIgnoreCase("ddo")){
-				return ddo.editRecord(managerId, recordID, filedname, newValue);
+				String request = "ddo|" + "ER" + "|" + managerId + "|"  + recordID + "|" + filedname + "|" + newValue ;
+				requestQ_DDO.add(request);
 			}
-			return "error of finding server";
+			return "request queued";
 		}
-
-	/**
-	 * depend on managerID, dicdie to call which center server.
-	 */
+	
+		/**
+		 * depend on managerID, dicdie to call which center server.
+		 */
 		@Override
-		public String transferRecord(String managerId, String recordID, String remoteCenterServerName){
+		public String transferRecord(String managerId, String recordID, String remoteCenterServerName) throws IOException {
 			if(managerId.substring(0, 3).equalsIgnoreCase("mtl")){
-				return mtl.transferRecord(managerId, recordID, remoteCenterServerName);
+				String request = "mtl|" + "TR" + "|" + managerId + "|"  + recordID + "|" + remoteCenterServerName ;
+				requestQ_MTL.add(request);
 			}
 			else if(managerId.substring(0, 3).equalsIgnoreCase("lvl")){
-				return lvl.transferRecord(managerId, recordID, remoteCenterServerName);
+				String request = "lvl|" + "TR" + "|" + managerId + "|"  + recordID + "|" + remoteCenterServerName ;
+				requestQ_LVL.add(request);
 			}
 			else if(managerId.substring(0, 3).equalsIgnoreCase("ddo")){
-				return ddo.transferRecord(managerId, recordID, remoteCenterServerName);
+				String request = "ddo|" + "TR" + "|" + managerId + "|"  + recordID + "|" + remoteCenterServerName ;
+				requestQ_DDO.add(request);
 			}
-			return "error of finding server";
+			return "request queued";
 		}
-
-//		/**
-//		 * depend on managerID, dicdie to call which center server.
-//		 */
-//		@Override
-//		public String createTRecord(String managerId, String firstName, String lastName, String address, String phone,
-//				String specialization, String location) throws IOException {
-//			if(managerId.substring(0, 3).equalsIgnoreCase("mtl")){
-//				String request = "mtl|" + "CT" + "|" + managerId + "|"  + firstName + "|" + lastName + "|" + address + "|" + phone + "|" + specialization + "|" + location;
-//				requestQ_MTL.add(request);
-//			}
-//			else if(managerId.substring(0, 3).equalsIgnoreCase("lvl")){
-//				String request = "lvl|" + "CT" + "|" + managerId + "|"  + firstName + "|" + lastName + "|" + address + "|" + phone + "|" + specialization + "|" + location;
-//				requestQ_LVL.add(request);
-//			}
-//			else if(managerId.substring(0, 3).equalsIgnoreCase("ddo")){
-//				String request = "ddo|" + "CT" + "|" + managerId + "|"  + firstName + "|" + lastName + "|" + address + "|" + phone + "|" + specialization + "|" + location;
-//				requestQ_DDO.add(request);
-//			}
-//			return "request queued";
-//		}
-	//
-	//
-//		/**
-//		 * depend on managerID, dicdie to call which center server.
-//		 */
-//		@Override
-//		public String createSRecord(String managerId, String firstName, String lastName, String courseRegistered,
-//				String status, String statusdate) throws IOException {
-//			if(managerId.substring(0, 3).equalsIgnoreCase("mtl")){
-//				String request = "mtl|" + "CS" + "|" + managerId + "|"  + firstName + "|" + lastName + "|" + courseRegistered + "|" + status + "|" + statusdate;
-//				requestQ_MTL.add(request);
-//			}
-//			else if(managerId.substring(0, 3).equalsIgnoreCase("lvl")){
-//				String request = "lvl|" + "CS" + "|" + managerId + "|"  + firstName + "|" + lastName + "|" + courseRegistered + "|" + status + "|" + statusdate ;
-//				requestQ_LVL.add(request);
-//			}
-//			else if(managerId.substring(0, 3).equalsIgnoreCase("ddo")){
-//				String request = "ddo|" + "CS" + "|" + managerId + "|"  + firstName + "|" + lastName + "|" + courseRegistered + "|" + status + "|" + statusdate;
-//				requestQ_DDO.add(request);
-//			}
-//			return "request queued";
-//		}
-	//
-	//
-//		/**
-//		 * depend on managerID, dicdie to call which center server.
-//		 */
-//		@Override
-//		public String getRecordCounts(String managerId) throws IOException {
-//			if(managerId.substring(0, 3).equalsIgnoreCase("mtl")){
-//				String request = "mtl|" + "RC" + "|" + managerId ;
-//				requestQ_MTL.add(request);
-//			}
-//			else if(managerId.substring(0, 3).equalsIgnoreCase("lvl")){
-//				String request = "lvl|" + "RC" + "|" + managerId ;
-//				requestQ_LVL.add(request);
-//			}
-//			else if(managerId.substring(0, 3).equalsIgnoreCase("ddo")){
-//				String request = "ddo|" + "RC" + "|" + managerId ;
-//				requestQ_DDO.add(request);
-//			}
-//			return "request queued";
-//		}
-	//
-	//
-//		/**
-//		 * depend on managerID, dicdie to call which center server.
-//		 */
-//		@Override
-//		public String editRecord(String managerId, String recordID, String filedname, String newValue) throws IOException {
-//			if(managerId.substring(0, 3).equalsIgnoreCase("mtl")){
-//				String request = "mtl|" + "ER" + "|" + managerId + "|"  + recordID + "|" + filedname + "|" + newValue ;
-//				requestQ_MTL.add(request);
-//			}
-//			else if(managerId.substring(0, 3).equalsIgnoreCase("lvl")){
-//				String request = "lvl|" + "ER" + "|" + managerId + "|"  + recordID + "|" + filedname + "|" + newValue ;
-//				requestQ_LVL.add(request);
-//			}
-//			else if(managerId.substring(0, 3).equalsIgnoreCase("ddo")){
-//				String request = "ddo|" + "ER" + "|" + managerId + "|"  + recordID + "|" + filedname + "|" + newValue ;
-//				requestQ_DDO.add(request);
-//			}
-//			return "request queued";
-//		}
-	//
-//		/**
-//		 * depend on managerID, dicdie to call which center server.
-//		 */
-//		@Override
-//		public String transferRecord(String managerId, String recordID, String remoteCenterServerName) throws IOException {
-//			if(managerId.substring(0, 3).equalsIgnoreCase("mtl")){
-//				String request = "mtl|" + "TR" + "|" + managerId + "|"  + recordID + "|" + remoteCenterServerName ;
-//				requestQ_MTL.add(request);
-//			}
-//			else if(managerId.substring(0, 3).equalsIgnoreCase("lvl")){
-//				String request = "lvl|" + "TR" + "|" + managerId + "|"  + recordID + "|" + remoteCenterServerName ;
-//				requestQ_LVL.add(request);
-//			}
-//			else if(managerId.substring(0, 3).equalsIgnoreCase("ddo")){
-//				String request = "ddo|" + "TR" + "|" + managerId + "|"  + recordID + "|" + remoteCenterServerName ;
-//				requestQ_DDO.add(request);
-//			}
-//			return "request queued";
-//		}
 
 }
 
